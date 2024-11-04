9f55501193703ffa082fdd01e11bcfb574c2c687
Tom Joy <thwjoy@gmail.com>
2024-11-04 11:52:22 +0000

    inserting audio tidy up

--- insert_audio.py
+++ insert_audio.py
@@ -12,6 +12,9 @@ from dtaidistance.dtw import warping_paths
 model = whisper.load_model("base").to("cuda" if torch.cuda.is_available() else "cpu")
 tokenizer = whisper.tokenizer.get_tokenizer(model.is_multilingual, language="English")
 
+def get_rms(waveform):
+    return np.sqrt(np.mean(waveform ** 2))
+
 def overlay_audio_with_wavfile(main_audio_path, words_df_path, timestamps_df_path, output_path):
     # Load the main audio file
     sample_rate, main_waveform = wavfile.read(main_audio_path)
@@ -26,10 +29,6 @@ def overlay_audio_with_wavfile(main_audio_path, words_df_path, timestamps_df_pat
     # Load dataframes
     words_df = pd.read_csv(words_df_path)
     timestamps_df = pd.read_csv(timestamps_df_path)
-
-    # # Ensure both dataframes have consistent whitespace and case
-    # words_df['Word'] = words_df['Word'].str.strip().str.lower()
-    # timestamps_df['word'] = timestamps_df['word'].str.strip().str.lower()
     
     # Convert timestamps in seconds to samples for placement
     timestamps_df['start_sample'] = (timestamps_df['begin'] * sample_rate).astype(int)
@@ -38,22 +37,15 @@ def overlay_audio_with_wavfile(main_audio_path, words_df_path, timestamps_df_pat
     # Create a copy of the main waveform to overlay sounds
     overlay_waveform = main_waveform.copy()
 
+    #  Ensure the 'word_tokens' column exists in words_df
+    if 'word_token' not in words_df.columns:
+        words_df['word_token'] = None
+
     # tokenize each word in words_df
-
-    # Ensure the 'word_tokens' column exists in words_df
-    # if 'word_token' not in words_df.columns:
-    #     words_df['word_token'] = None
-
-    # for idx, row in words_df.iterrows():
-    #     if pd.notnull(row['Word']) and row['Word'].strip() != '':
-    #         tokens = tokenizer.encode(" " + row['Word']) # TODO fix this
-    #         words_df.at[idx, 'word_token'] = tokens
-    #         if row['Sound Description'] != 'silence':
-    #             words_df.at[idx, 'audio'] = f"harry_potter_1_db_v2/sounds/audio_{idx+2}.wav"
-
-    # # save words_df
-    # words_df.to_csv('words_df.csv', index=False)
-    words_df = pd.read_csv('words_df.csv')
+    for idx, row in words_df.iterrows():
+        if pd.notnull(row['Word']) and row['Word'].strip() != '':
+            tokens = tokenizer.encode(" " + row['Word']) # TODO fix this
+            words_df.at[idx, 'word_token'] = tokens
 
     if 'begin' not in words_df.columns:
         words_df['begin'] = None
@@ -61,32 +53,23 @@ def overlay_audio_with_wavfile(main_audio_path, words_df_path, timestamps_df_pat
     if 'end' not in words_df.columns:
         words_df['end'] = None
 
-    # loop through all rows of words_df, find where the seqnece matches in timestamps,
-    # then overlay the sound effect
-
     def flattern_list(list):
         return [item for sublist in list if sublist is not None for item in sublist if item is not None]
 
-    # words_toks = flattern_list(words_df['word_token'].tolist())
-    # timestamps_toks = flattern_list(timestamps_df['word_token'].apply(ast.literal_eval).tolist())
-
     # # get the index in timestamps where this audio occurs
-    # _, paths = warping_paths(words_toks, timestamps_toks, psi=0) # TODO fix this
-
-    # # save paths
-    # np.save('paths.npy', paths)
-    paths = np.load('paths.npy')
+    words_toks = flattern_list(words_df['word_token'].tolist())
+    timestamps_toks = flattern_list(timestamps_df['word_token'].apply(ast.literal_eval).tolist())
+    _, paths = warping_paths(words_toks, timestamps_toks, psi=0) # TODO fix this
+    # save paths
+    # np.save("harry_potter_1_db_v2/paths.py", paths)
+    # paths = np.load("harry_potter_1_db_v2/paths.npy")
 
     token_count = 0
 
-    last_end_sample = 0
     for idx, row in words_df.iterrows():
-        token_count += len(ast.literal_eval(row['word_token'])) if pd.notnull(row['word_token']) else 0
+        token_count += len(row['word_token']) if row['word_token'] is not None else 0
         if row['Sound Description'] != 'silence':
             # find where this word fits in timestamps
-            # if idx > 100:
-            #     break
-
             current_token_timestamp_idx = int(np.argmin(paths[token_count, :]))
             # we now need to get the timestamp index for this token_count in timestamps_df
             current_timestamp_idx = timestamps_df.index[timestamps_df['word_token'].apply(ast.literal_eval).apply(len).cumsum() >= current_token_timestamp_idx][0]
@@ -121,20 +104,20 @@ def overlay_audio_with_wavfile(main_audio_path, words_df_path, timestamps_df_pat
                 end_sample = main_audio_length
                 sound_waveform = sound_waveform[:end_sample - start_sample, :]
 
-            # if start_sample > last_end_sample:
-            overlay_waveform[start_sample:end_sample, :] += sound_waveform
+            # normalise sound_waveform 
+            amplitude = 0.7 * np.max(overlay_waveform[start_sample:end_sample]) / np.max(sound_waveform)
+            overlay_waveform[start_sample:end_sample, :] += np.astype(sound_waveform * amplitude, np.int16)
 
-            last_end_sample = end_sample
-
-    # Save the output
-    words_df.to_csv('words_df.csv', index=False)
     wavfile.write(output_path, sample_rate, overlay_waveform)
 
 def main():
+    # python insert_audio.py --main_audio_path harry_potter_1_db_v2/chapter_1_narration.wav --words_df_path harry_potter_1_db_v2/harry_potter_1_db_v2_descriptions.csv --timestamps_df_path harry_potter_1_db_v2/audio_timestamps.csv --output_path test.wav
     parser = argparse.ArgumentParser(description="Overlay sound effects onto an audio file based on word timestamps.")
     parser.add_argument("--main_audio_path", type=str, help="Path to the main audio file")
     parser.add_argument("--words_df_path", type=str, help="Path to the CSV file containing words and audio file paths")
+    # needs to have columns Word,Sound Description,audio
     parser.add_argument("--timestamps_df_path", type=str, help="Path to the CSV file containing word timestamps")
+    # needs to have columns word,word_token,begin,end,diff
     parser.add_argument("--output_path", type=str, help="Path to save the output audio file with overlays")
 
     args = parser.parse_args()
@@ -147,4 +130,5 @@ def main():
     )
 
 if __name__ == "__main__":
+
     main()
